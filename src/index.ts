a84c9addc1715ac00f011f0eccab8c4c1a3ac899
Dan Goodman <danthegoodman@icloud.com>
2021-07-18 13:26:33 -0400

    Lots more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import NodeRSA = require('node-rsa')
 import env from './utils/env'
 import path from 'path'
-import { TaskContract } from './types';
+import { ActorCredentials, TaskContract } from './types';
+import { Contract } from '@balena/jellyfish-types/build/core';
+import registry from './lib/registry';
 
 const secretsKey = env.secretKey ?
 	new NodeRSA(Buffer.from(env.secretKey, 'base64').toString('utf-8'), 'pkcs1', { encryptionScheme: 'pkcs1' })
@@ -12,6 +14,77 @@ const directory = {
     output: (task: TaskContract) => path.join(env.outputDir, `task-${task.id}`),
   };
 
+function createArtifactReference ({ slug, version }: Contract) {
+	let registryPort = "";
+	if (env.registryPort) {
+		registryPort = `:${env.registryPort}`;
+	}
+	return `${env.registryHost}${registryPort}/${slug}:${version}`;
+}
+
+  async function pullTransformer(
+    task: TaskContract,
+    actorCredentials: ActorCredentials,
+  ) {
+    const transformerImageReference = createArtifactReference(task.data.transformer);
+    console.log(`[WORKER] Pulling transformer ${transformerImageReference}`);
+    const transformerImageRef = await registry.pullImage(
+      transformerImageReference,
+      { username: actorCredentials.slug, password: actorCredentials.sessionToken },
+    );
+
+    return transformerImageRef;
+  }
+
+  async function runTransformer(task: TaskContract, transformerImageRef: string) {
+    console.log(`[WORKER] Running transformer image ${transformerImageRef}`);
+
+    const docker = registry.docker;
+
+    // docker-in-docker work by mounting a tmpfs for the inner volumes
+    const tmpDockerVolume = `tmp-docker-${task.id}`;
+
+    //HACK - dockerode closes the stream unconditionally
+    process.stdout.end = () => { }
+    process.stderr.end = () => { }
+
+    const runResult = await docker.run(
+      transformerImageRef,
+      [],
+      [process.stdout, process.stderr],
+      {
+        Tty: false,
+        Env: [
+          `INPUT=/input/${env.inputManifestFilename}`,
+          `OUTPUT=/output/${env.outputManifestFilename}`,
+        ],
+        Volumes: {
+          '/input/': {},
+          '/output/': {},
+          '/var/lib/docker': {} // if the transformers uses docker-in-docker, this is required
+        },
+        HostConfig: {
+          Init: true, // should ensure that containers never leave zombie processes
+          Privileged: true, //TODO: this should at least only happen for Transformers that need it
+          Binds: [
+            `${path.resolve(directory.input(task))}:/input/:ro`,
+            `${path.resolve(directory.output(task))}:/output/`,
+            `${tmpDockerVolume}:/var/lib/docker`,
+          ],
+        },
+      } as ContainerCreateOptions,
+    );
+
+    const output = runResult[0];
+    const container = runResult[1];
+
+    await docker.getContainer(container.id).remove({force: true})
+    await docker.getVolume(tmpDockerVolume).remove({force: true})
+
+    console.log("[WORKER] run result", JSON.stringify(runResult));
+
+    return output.StatusCode;
+  }
 
 /**
  * This function takes an object tree with all string values expected to be
---
+++ src/utils/helpers.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs';
+import { F_OK } from 'constants';
+import { Formula } from '../types';
+const jellyscript = require('@balena/jellyfish-jellyscript');
+
+
+export const pathExists = async (path: string) => {
+    try {
+        await fs.promises.access(path, F_OK);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+export function streamToPromise(stream: NodeJS.ReadableStream): Promise<string> {
+    return new Promise((resolve, reject) => {
+        let buf = '';
+        stream.on('data', (d) => (buf += d.toString()));
+        stream.on('end', () => resolve(buf));
+        stream.on('error', reject);
+    });
+}
+
+export function evaluateFormulaOrValue(formulaOrValue: Formula | any, context: any) {
+    if(formulaOrValue.$$formula) {
+        try {
+            const result = jellyscript.evaluate(formulaOrValue.$$formula, {
+                context
+            });
+            return result?.value;
+        }
+        catch(e) {
+            if(e.message) {
+                e.message = `Formula eval error: ${e.message}`;
+            }
+            throw e;
+        }
+    } else {
+        return formulaOrValue;
+    }
+}
