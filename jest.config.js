4d8394d3715f22ef85ba272782a8b96da730d0b1
balena-ci <balena-ci@users.noreply.github.com>
2021-08-05 11:50:33 +0000

    deploy: 43d9b3f5a5fea7e385b28ebd5175dc0404a6ac72

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-const base = require("@balena/jellyfish-config/config/jest.config")
-
-module.exports = {
-  ...base,
-  testTimeout: 30000,
-}
--- test/integration/integration.spec.ts
+++
@@ -1,6 +0,0 @@
-import { integrationTest } from '.'
-
-test('Integration test', async () => {
-  const result = await integrationTest()
-  return expect(result).toBe(undefined)
-})
